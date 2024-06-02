2e86523bf46915fddc59901a6d3bcfc8286032c2
marcinoles87 <marcinoles87@gmail.com>
2024-06-02 11:47:20 +0200

    add new section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 
 import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
    
       <Header></Header>
+      <Nav></Nav>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className='nav-container'>
+      <div className='nav-items'>
+        <ul>
+          <li><a href='#'>Główna</a></li>
+          <li><a href='#'>I Drużyna</a></li>
+          <li><a href='#'>Akademia</a></li>
+          <li><a href='#'>Galeria</a></li>
+          <li><a href='#'>Rozgrywki</a></li>
+          <li><a href='#'>Kontakt</a></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
