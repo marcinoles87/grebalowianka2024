da3c322fbd3b44557b9c10ed16bbd4f5ae406ae2
marcinoles87 <marcinoles87@gmail.com>
2024-06-07 15:10:30 +0200

    add section

---
+++ src/components/Kontakt.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Kontakt() {
+  return (
+    <div className='contact-container'>
+        <h1>Kontakt</h1>
+        <div className='contact-item'>
+            <p>Klub Sportowy Grębałowianka</p>
+        </div>
+    </div>
+  )
+}
+
+export default Kontakt
