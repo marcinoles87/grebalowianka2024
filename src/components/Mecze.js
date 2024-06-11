d666d12ae3a7822f75c739cccf4a9ce7321cbacf
marcinoles87 <marcinoles87@gmail.com>
2024-06-11 15:34:56 +0200

    add section

---
+++ src/components/Mecze.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Mecze() {
+
+    const matchDay = [
+        {
+            goście :'' ,
+            gospodarze : '' ,
+            godzinaMeczu : '' ,
+            wynik : '' ,
+            
+
+        }
+    ]
+
+
+  return (
+    <div className='mecze-container'>
+
+        </div>
+  )
+}
+
+export default Mecze
