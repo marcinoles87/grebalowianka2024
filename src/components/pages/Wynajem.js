8c25d905fb402779889d7b87b3bebba1a3c9bda6
marcinoles87 <marcinoles87@gmail.com>
2025-09-10 20:00:06 +0200

    add links and new pages

--- src/components/pages/Wynajem.js
+++ src/components/pages/Wynajem.js
@@ -13,14 +13,29 @@ function Wynajem() {
 
       <div class="boiska">
 
+      <div class="male">
         <h2>Małe boisko</h2>
         <img src={małe} alt='male'></img>
-
-        <h2>Duże boisko</h2>
+        <p>90min - 500zl</p>
+        <p>60min - 300zl</p>
+      </div>
+        
+      <div class="male">
+         <h2>Duże boisko</h2>
         <img src={duze} alt='male'></img>
-
-        <h2>Kort tenisowy</h2>
+        <p>90min - 700zl</p>
+        <p>60min - 400zl</p>
+      </div>
+       
+      <div class="male">
+         <h2>Kort tenisowy</h2>
         <img src={kort} alt="kort"></img>
+        <p>90min - 200zl <br></br>
+          60min -100zl
+        </p>
+        
+      </div>
+       
 
       </div>
     </div>
