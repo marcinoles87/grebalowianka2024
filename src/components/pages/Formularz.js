7d5c4770d492ad93e8c0c185377c56a1c6ecc821
marcinoles87 <marcinoles87@gmail.com>
2025-09-15 15:47:45 +0200

    add links and new pages

--- src/components/pages/Formularz.js
+++ src/components/pages/Formularz.js
@@ -4,7 +4,20 @@ function Formularz() {
   return (
     <div className='formularz-container'>
       <div className='form'>
-        <form action="send"></form>
+        <form action="send">
+
+          <h1>Wyślij nam zapytanie </h1>
+
+          <label for="name">Name:</label>
+          <input type="text" placeholder='name'/>
+
+          <label for="email">Email:</label>
+          <input type="email" placeholder='email'/>
+
+          <label for="message">Message: </label>
+          <input type="text" placeholder='message'/>
+
+        </form>
       </div>
 
     </div>
