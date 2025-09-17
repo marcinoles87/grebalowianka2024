d87f1734002aaf54117465e30cf6a42b7eee0574
marcinoles87 <marcinoles87@gmail.com>
2025-09-17 16:32:57 +0200

    add links and new pages

--- src/components/pages/Formularz.js
+++ src/components/pages/Formularz.js
@@ -5,18 +5,27 @@ function Formularz() {
 const [name , setName] = useState('');
 const [email , setEmail] = useState('');
 const [message , setMessage] = useState('');
-const [placholder , setPlaceholder] = useState(false)
+const [placholderError , setPlaceholder] = useState(false)
 
 const sendEmail = () =>{
 
+  const names = document.querySelector('#name')
+  const emails = document.querySelector('#email')
+  const messages = document.querySelector('#message')
 
-if(name.length < 3){
+
+if(name.length < 3 || email.length < 2 || message.length < 1) {
   alert(`Imie zbyt krótkie , prosimy wpisac ponownie`)
   setPlaceholder(true)
 }else{
 
-  alert(`Hello ${name} , thanks for your message , We feedback as soon as possible ! team Greba . Wiadomość wysłana`)
+  alert(`Hello ${name} , thanks for your message , We feedback on ${email} as soon as possible ! team Greba . Wiadomość wysłana`)
+  setPlaceholder(false)
 
+  names.value = '';
+  emails.value = '';
+  messages.value = '';
+  
 }
 
 }
@@ -30,13 +39,13 @@ console.log(name)
           <h1>Wyślij nam zapytanie </h1>
 
           <label htmlFor="name">Name:</label>
-          <input id='name' type="text" placeholder={placholder ? 'name is to short !' : 'name'} autoComplete='off' onChange={ (e) => { setName(e.target.value)}}/>
+          <input id='name' type="text" placeholder={placholderError ? 'name is to short !' : 'name'} autoComplete='off' onChange={ (e) => { setName(e.target.value)}}/>
 
           <label htmlFor="email">Email:</label>
-          <input id='email' type="email" placeholder='email' autoComplete='off' onChange={ (e) => { setEmail(e.target.value)}}/>
+          <input id='email' type="email" placeholder={placholderError ? 'email is to short !' : 'email'} autoComplete='off' onChange={ (e) => { setEmail(e.target.value)}}/>
 
           <label htmlFor="message">Message: </label>
-          <textarea id='message' type="text" placeholder='send as a message...' autoComplete='off' onChange={ (e) => { setMessage(e.target.value)}}/>
+          <textarea id='message' type="text" placeholder={placholderError ? 'message is to short !' : 'message'} autoComplete='off' onChange={ (e) => { setMessage(e.target.value)}}/>
 
         </form>
 
