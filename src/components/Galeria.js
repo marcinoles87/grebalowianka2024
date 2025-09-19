fb8ba9989d21368684c3385d5bb2657e630af070
marcinoles87 <marcinoles87@gmail.com>
2025-09-19 16:26:48 +0200

    add links and new pages

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,49 +1,14 @@
-import React from 'react'
 
-import gre_m1 from './imgMecze/gre_1.jpg'
 
 
 function Galeria() {
-
-    const gal =  [
-        {
-            img1: gre_m1,
-            text:'Grębałowianka-Prądniczanka'
-          
-        } ,
-
-        {
-            img1: gre_m1,
-            text:'Grębałowianka-Proszowianka'
-          
-        } ,
-
-        {
-            img1: gre_m1,
-            text:'Grębałowianka-Pogoń Miechów'
-          
-        }
-    ]
   return (
     <div>
         <div className='galeria-container'>
             <h1>Galeria</h1>
-            <div className='galeria-items' id='galeria'>
-               
-                {gal.map( (item ,index) => {
-                    return(
-                        <div className='galeria-item' key={index}>
-                            <img src={item.img1} alt=""></img>
-                            <p>{item.text}</p>
-                            </div>
-                        
-                    )
-                })}
-                
-                    
-                
+        
             </div>
-        </div>
+        
     </div>
   )
 }
